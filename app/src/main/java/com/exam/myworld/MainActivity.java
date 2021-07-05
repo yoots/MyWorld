73555e334cea02717cf62f602940f0dfcc6950ec
gundallove <gundallove@gmail.com>
2021-07-05 16:13:19 +0900

    메인 액티비티 수정.

--- app/src/main/java/com/exam/myworld/MainActivity.java
+++ app/src/main/java/com/exam/myworld/MainActivity.java
@@ -3,6 +3,9 @@ package com.exam.myworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //다시 커밋 합니다.
-        Toast.makeText(this, "1111" , Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "효비는 짱임!!" , Toast.LENGTH_LONG).show();
+
+        //Button btn = (Button)
+    }
+
+    public void onClickButton(View v){
+
+        Log.d("exam" , "onClickButton");
+        Toast.makeText(this, "효비는 짱임!!" , Toast.LENGTH_LONG).show();
     }
 }
